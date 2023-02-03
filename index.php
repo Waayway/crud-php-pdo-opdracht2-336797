3ccf6b6f459f0ecf006ee3d7bb5a652c6305bfb3
Waayway <thijs@vanwaaij.com>
2023-02-03 09:22:21 +0100

    css classes toegevoegd

--- index.php
+++ index.php
@@ -11,41 +11,49 @@
 
 <body>
     <h1>Maak zelf je pizza</h1>
-    <form action="" method="post">
-        <select name="bodem">
-            <option value="keuze">Maak je keuze</option>
-            <?php
-            $bodemOptions = array(20, 25, 30, 35, 40);
-            foreach ($bodemOptions as $value) {
-                echo "<option value='$value'>$value Centimeter</option>";
-            }
-            ?>
-        </select>
-        <select name="saus">
-            <option value="keuze">Maak je keuze</option>
-            <?php
-            $sausOptions = array(
-                "Tomatensaus", "Extra tomatensaus", "Spicy tomatensaus", "BBQ saus", "Crème fraiche"
-            );
-            foreach ($sausOptions as $value) {
-                echo "<option value='$value'>$value</option>";
-            }
-            ?>
-        </select>
-        <?php 
-        $toppingOptions = array("Vegan", "Vegetarisch", "Vlees");
-        foreach ($toppingOptions as $value) {
-            echo "<input type='radio' name='topping' value='$value' id='$value'><label for='$value'>$value</label>";
-        }
-        ?>
-        <?php 
-        $kruidenOptions = array("Peterselie", "Oregano", "Chili flakes", "Zwarte peper");
-        foreach ($kruidenOptions as $value) {
-            echo "<input type='checkbox' name='kruiden' value='$value' id='$value'><label for='$value'>$value</label>";
-        }
-        ?>
-        <button type="submit">Bestel</button>
-    </form>
+    <section id="form" class="col-12">
+        <div class="content">
+            <form action="" method="post">
+                <h3>Bodemformaat</h3>
+                <select name="bodem">
+                    <option value="keuze">Maak je keuze</option>
+                    <?php
+                    $bodemOptions = array(20, 25, 30, 35, 40);
+                    foreach ($bodemOptions as $value) {
+                        echo "<option value='$value'>$value Centimeter</option>";
+                    }
+                    ?>
+                </select>
+                <h3>Saus</h3>
+                <select name="saus">
+                    <option value="keuze">Maak je keuze</option>
+                    <?php
+                    $sausOptions = array(
+                        "Tomatensaus", "Extra tomatensaus", "Spicy tomatensaus", "BBQ saus", "Crème fraiche"
+                    );
+                    foreach ($sausOptions as $value) {
+                        echo "<option value='$value'>$value</option>";
+                    }
+                    ?>
+                </select>
+                <h3>Toppings</h3>
+                <?php
+                $toppingOptions = array("Vegan", "Vegetarisch", "Vlees");
+                foreach ($toppingOptions as $value) {
+                    echo "<div><input type='radio' name='topping' value='$value' id='$value'><label for='$value'>$value</label></div>";
+                }
+                ?>
+                <h3>Kruiden</h3>
+                <?php
+                $kruidenOptions = array("Peterselie", "Oregano", "Chili flakes", "Zwarte peper");
+                foreach ($kruidenOptions as $value) {
+                    echo "<div><input type='checkbox' name='kruiden' value='$value' id='$value'><label for='$value'>$value</label></div>";
+                }
+                ?>
+                <button type="submit">Bestel</button>
+            </form>
+        </div>
+    </section>
 </body>
 
 </html>
