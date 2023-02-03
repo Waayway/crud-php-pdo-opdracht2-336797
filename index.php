bd4ea67239f77e454d7804676b03667863429b9d
Waayway <thijs@vanwaaij.com>
2023-02-03 09:12:45 +0100

    bijde select elementen aangemaakt met gebruik van PHP

--- index.php
+++ index.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maak zelf je pizza</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
+
 <body>
     <h1>Maak zelf je pizza</h1>
+    <form action="" method="post">
+        <select name="bodem">
+            <option value="keuze">Maak je keuze</option>
+            <?php
+            $bodemOptions = array(20, 25, 30, 35, 40);
+            foreach ($bodemOptions as $value) {
+                echo "<option value='$value'>$value Centimeter</option>";
+            }
+            ?>
+        </select>
+        <select name="saus">
+            <option value="keuze">Maak je keuze</option>
+            <?php
+            $sausOptions = array(
+                "Tomatensaus", "Extra tomatensaus", "Spicy tomatensaus", "BBQ saus", "Crème fraiche"
+            );
+            foreach ($sausOptions as $value) {
+                echo "<option value='$value'>$value</option>";
+            }
+            ?>
+        </select>
+    </form>
 </body>
+
 </html>
